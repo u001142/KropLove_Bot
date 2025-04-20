12e35b0c5f03e26ebe14ccbdebfaa836bf2de7b5
u001142 <victorgrabenko@gmail.com>
2025-04-21 01:31:25 +0300

    Update init.py

--- app/models/init.py
+++ app/models/init.py
@@ -1,1 +1,9 @@
+from app.models.user import User
+from app.models.like import Like
+from app.models.match import Match
+from app.models.complaint import Complaint
+from app.models.premium import Premium
+from app.models.referral import Referral
 
+# Якщо буде потрібно автоматично створити всі таблиці
+from app.database import Base
