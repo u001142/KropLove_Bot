66abdedb5f5164ef8a35371eabda6c5a6ac19515
u001142 <victorgrabenko@gmail.com>
2025-04-21 01:26:16 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,12 @@
-# SQLAlchemy DB connection
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import DATABASE_URL
+
+# Створення SQLAlchemy-двигуна (engine)
+engine = create_engine(DATABASE_URL, echo=False)
+
+# Сесія для виконання запитів
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Базовий клас для моделей
+Base = declarative_base() 
