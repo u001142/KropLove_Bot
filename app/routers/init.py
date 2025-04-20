3d40909d77021e6a219ae224e5aba9f79825624f
u001142 <victorgrabenko@gmail.com>
2025-04-21 01:34:09 +0300

    Update init.py

--- app/routers/init.py
+++ app/routers/init.py
@@ -1,1 +1,13 @@
+from app.routers.start_router import router as start_router
+from app.routers.profile_router import router as profile_router
+from app.routers.like_router import router as like_router
+from app.routers.chat_router import router as chat_router
+from app.routers.premium_router import router as premium_router
 
+all_routers = [
+    start_router,
+    profile_router,
+    like_router,
+    chat_router,
+    premium_router
+]
