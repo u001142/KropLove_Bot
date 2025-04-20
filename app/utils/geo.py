f35f2cae4553e0619912f24bd872afc213742633
u001142 <victorgrabenko@gmail.com>
2025-04-21 02:21:38 +0300

    Update geo.py

--- app/utils/geo.py
+++ app/utils/geo.py
@@ -1,1 +1,19 @@
+import math
 
+def calculate_distance_km(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    """
+    Обчислює відстань між двома точками (GPS) у кілометрах
+    """
+    R = 6371  # Радіус Землі в км
+
+    d_lat = math.radians(lat2 - lat1)
+    d_lon = math.radians(lon2 - lon1)
+    a = (
+        math.sin(d_lat / 2) ** 2 +
+        math.cos(math.radians(lat1)) *
+        math.cos(math.radians(lat2)) *
+        math.sin(d_lon / 2) ** 2
+    )
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    return round(distance, 2)
