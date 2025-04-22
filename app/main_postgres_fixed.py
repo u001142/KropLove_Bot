ad867138548c5e53ac945ec43104c3c60735759f
u001142 <victorgrabenko@gmail.com>
2025-04-22 09:25:48 +0300

    Update main_postgres_fixed.py

--- app/main_postgres_fixed.py
+++ app/main_postgres_fixed.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 app = FastAPI()
 
-DB setup
+# DB setup
 
 DATABASE_URL = os.getenv("DATABASE_URL") engine = create_engine(DATABASE_URL) SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) Base = declarative_base()
 
